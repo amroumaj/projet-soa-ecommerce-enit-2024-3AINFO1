daeb36de4d1b61d3c3a9de4e7bd2aee94a38d8e3
Sakly Rami <123549194+Ramisk2000@users.noreply.github.com>
2024-11-10 20:03:10 +0100

    first commit

---
+++ catalog/models/Product.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index');
+
+const Product = sequelize.define('Product', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
