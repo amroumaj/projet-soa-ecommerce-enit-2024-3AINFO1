aff61bcd587375838fafaa50714932bea3c0d020
Sakly Rami <123549194+Ramisk2000@users.noreply.github.com>
2024-11-22 02:07:07 +0100

    stop tracking other direcs

--- pricing/demo/src/main/java/com/example/demo/Repository/PromotionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Models.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-    List<Promotion> findByProductId(Long productId);
-}
